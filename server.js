e14ba1b04c1b47873e1d24b3a3aacaa1a7a2e9d8
awaisamir123 <awaisamir123@gmail.com>
2023-01-11 17:21:56 +0500

    dsasd

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ import UserRoutes from './routes/UserRoutes.js';
 import VerificationRoutes from './routes/VerificationRoutes.js';
 import ItemsRoutes from './routes/ItemsRoutes.js'
 
-import path from  'path'
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// import path from  'path'
+// import { fileURLToPath } from 'url';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 
 var jsonParser = bodyParser.json()
@@ -21,10 +21,10 @@ const app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
-app.use(express.static(path.resolve(__dirname, './build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './build', 'index.html'));
-});
+// app.use(express.static(path.resolve(__dirname, './build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './build', 'index.html'));
+// });
 
 app.use(express.json());
 app.use(bodyParser.json())
